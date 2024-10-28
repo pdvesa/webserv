e496b5a155e0ab0cf1bec7faaef953385d592cb9
jcayot <jules.cayot@icloud.com>
2024-10-28 15:06:40 +0200

    Add test for parsing multiple servers from config file
    This commit introduces a new test case to ensure that the `ServerConfig` class correctly parses configurations containing multiple server blocks from a file. Additionally, it refactors the test setup by adding a helper function to create temporary configuration files, facilitating easier and more organized test management.

--- test/model/ServerConfigTest.cpp
+++ test/model/ServerConfigTest.cpp
@@ -6,8 +6,48 @@
 #include <gtest/gtest.h>
 #include <ServerConfig.hpp>
 
+const std::string CONFIG_FILE_NAME = "configfile.example";
+const std::string CONFIG_FILE_CONTENT = "server { hostname www.alpaca.com\n"
+						   "port 1\n\n"
+						   "server_name alpaca lama\n"
+						   "error_page 404 405 /error/general.html\n\n"
+						   "client_max_body_size 2\n\n"
+						   "location /saku/ {\n"
+						   "    methods GET POST\n"
+						   "    listing on\n"
+						   "    root /svesa/\n"
+						   "}}\n"
+
+"server { hostname www.lama.com\n"
+						   "port 1\n\n"
+						   "server_name alpaca lama\n"
+						   "error_page 404 405 /error/general.html\n\n"
+						   "client_max_body_size 2\n\n"
+						   "location /saku/ {\n"
+						   "    methods GET POST\n"
+						   "    listing on\n"
+						   "    root /svesa/\n"
+						   "}}\n"
+
+"server { hostname www.peru.com\n"
+						   "port 1\n\n"
+						   "server_name alpaca lama\n"
+						   "error_page 404 405 /error/general.html\n\n"
+						   "client_max_body_size 2\n\n"
+						   "location /saku/ {\n"
+						   "    methods GET POST\n"
+						   "    listing on\n"
+						   "    root /svesa/\n"
+						   "}}\n";
+
+void createTempFile(const std::string& filePath, const std::string& content) {
+	std::ofstream file(filePath);
+	file << content;
+	file.close();
+}
+
 TEST(ServerConfigParseServerTestSuite, SimpleBlock) {
-	std::string serverBlock = "server{ hostname www.alpaca.com\n"
+	std::string serverBlock = "server { hostname www.alpaca.com\n"
 						   "port 1\n\n"
 						   "server_name alpaca lama\n"
 						   "error_page 404 405 /error/general.html\n\n"
@@ -28,4 +68,14 @@ TEST(ServerConfigParseServerTestSuite, SimpleBlock) {
 	ASSERT_STREQ(config.getErrorsPages().at(405).c_str(), "/error/general.html");
 	ASSERT_EQ(config.getMaxClientBodySize(), 2);
 	ASSERT_EQ(config.getRoutes().size(), 1);
+}
+
+TEST(ServerConfigFromConfigFileTestSuite, SimpleConfigFile) {
+	std::ofstream file(CONFIG_FILE_NAME);
+	if (!file.is_open())
+		FAIL() << "Failed to open config file code might be right";
+	file << CONFIG_FILE_CONTENT;
+	file.close();
+	std::vector<ServerConfig> serverConfigs = ServerConfig::fromConfigFile(CONFIG_FILE_NAME);
+	ASSERT_EQ(serverConfigs.size(), 3);
 }
