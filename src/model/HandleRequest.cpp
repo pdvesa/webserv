74fdf7a86d13cdc5d10b42ad332e5bdf2bbd4a1b
jules <jules.cayot@ik.me>
2024-12-02 19:39:35 +0200

    Bad requests

--- src/model/HandleRequest.cpp
+++ src/model/HandleRequest.cpp
@@ -5,26 +5,62 @@
 #include <HandleRequest.hpp>
 
 std::string HandleRequest::handleGet(const std::string& location) {
+	if (isDirectory(location)) {
+		return (listingBody(location));
+	}
 	std::ifstream	targetFile(location);
 
-	if (!targetFile.is_open())
+	if (!targetFile.is_open()) {
 		throw std::runtime_error("File not found");
+	}
 	std::stringstream buffer;
 	buffer << targetFile.rdbuf();
 	targetFile.close();
 	return (buffer.str());
 }
 
-void HandleRequest::handleDelete(const std::string& location) {
+std::string HandleRequest::handleDelete(const std::string& location) {
 	if (remove(location.c_str()) != 0)
 		throw std::runtime_error("File could not be deleted");
+	return ("");
 }
 
-void HandleRequest::handlePost(const std::string& location, const std::string& content) {
+std::string HandleRequest::handlePost(const std::string& location, std::vector<unsigned char>& content) {
 	std::ofstream	targetFile(location);
 
 	if (!targetFile.is_open())
 		throw std::runtime_error("Cannot create file at location");
-	targetFile << content;
+	for (const unsigned char c : content)
+		targetFile << c;
 	targetFile.close();
+	return ("");
+}
+
+bool	HandleRequest::isDirectory(const std::string& path) {
+	struct stat statbuf;
+
+	if (stat(path.c_str(), &statbuf) != 0)
+		return (false);
+	return (S_ISDIR(statbuf.st_mode));
+}
+
+std::string HandleRequest::listingBody(const std::string& location) {
+	std::ostringstream	content;
+	DIR					*directory;
+	struct dirent		*entry;
+
+	if ((directory = opendir(location.c_str())) != nullptr) {
+		content << "<html><body><h1>Directory listing for " << location << "</h1><ul>";
+		while ((entry = readdir(directory)) != nullptr) {
+			std::string name = entry->d_name;
+			if (name != "." && name != "..") {
+				content << "<li><a href=\"" << location << "/" << name << "\">" << name << "</a></li>";
+			}
+		}
+		closedir(directory);
+		content << "</ul></body></html>";
+	} else {
+		throw std::runtime_error("Could not open directory");
+	}
+	return (content.str());
 }
