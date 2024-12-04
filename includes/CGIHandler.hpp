1488d98d4f583b13f89f544bce96e6379d0ab614
pdvesa <saku.vesa@gmail.com>
2024-12-04 18:43:15 +0200

    some cgi parent child co-operation

--- includes/CGIHandler.hpp
+++ includes/CGIHandler.hpp
@@ -5,12 +5,15 @@
 #include <string>
 #include <filesystem>
 #include <unistd.h>
+#include <sys/wait.h>
 #include <HttpRequest.hpp>
+#include <Client.hpp>
 
 class CGI {
 	private:
 		std::vector<std::string>	env;
 		std::vector<char *>			envp;
+		std::string					cgiResponse;
 	public:
 		CGI();
 		~CGI();
--- src/model/CGIHandler.cpp
+++ src/model/CGIHandler.cpp
@@ -7,8 +7,10 @@ CGI::~CGI() {
 }
 
 void CGI::runCGI(HttpRequest req) {
-	pid_t pid = fork();
-	char *args[3];
+	pid_t	pid;
+	int		pipes[2];
+	char	*args[3];
+
 	std::string	script = req.getPath();
 	switch (req.getEnum()) {
 		case py:
@@ -20,14 +22,36 @@ void CGI::runCGI(HttpRequest req) {
 			args[1] = nullptr;
 			break ;
 		default:
-			std::cout << "This should never happen, segfaulting next... :)";
+			std::cout << "This should never happen, segfaulting next... :)" << std::endl;
 	}
 	args[2] = nullptr;
-	//pipe to tell back to parent what we just did
-	if (pid == 0) 
-        execve(args[0], args, envp.data());
-	else 
-        waitpid(pid, nullptr, 0);
+	if (pipe(pipes) == -1)
+		//do stuff
+	pid = fork();
+	if (pid == -1)
+		//do stuff
+	if (pid == 0) {
+        close(pipes[0]);
+		if (dup2(pipes[1], STDOUT_FILENO) == -1)
+			//do stuff
+		close(pipes[1]);
+		execve(args[0], args, envp.data());
+		//error do stuff
+	}
+	else {
+        close(pipes[1]);
+		int rv;
+		std::vector<unsigned char>	buffer(BUF_SIZE);
+		while ((rv = read(pipes[0], buffer.data(), BUF_SIZE)) > 0) {
+			buffer.resize(rv);
+			cgiResponse.append(buffer.begin(), buffer.end());
+			if (rv < BUF_SIZE) 
+				break;
+		}
+		close(pipes[0]);
+		waitpid(pid, nullptr, 0);
+		//timeout 
+	} 
 }
 
 void CGI::fillEnv(HttpRequest req) { //debug this shit if these are even correct like getHost
@@ -42,6 +66,9 @@ void CGI::fillEnv(HttpRequest req) { //debug this shit if these are even correct
 	env.push_back("SCRIPT_NAME=" + req.getPath()); 
 	env.push_back("PATH_INFO=" + std::filesystem::current_path().string() + req.getPath()); //not sure what goes here
 //	env.push_back("REMOTE_ADDR="); //dont know if needed (client addr) i dont think we even save it anymore :)
+	std::vector temp = req.getBody();
+	std::string	body(temp.begin(), temp.end());
+	env.push_back("POST_BODY=" + body); //lets hope that converting doessnt break anything
 	for (auto var : env) {
 		envp.push_back(var.data()); //make sure you dont die
 	}
